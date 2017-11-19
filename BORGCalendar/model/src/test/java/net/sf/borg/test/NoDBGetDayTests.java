cff47ac44aa5a25d6832466c689011f027b42b38
Matt Smith <mjsmith17@bsu.edu>
2017-11-18 19:14:06 -0500

    Update to NoDBGetDayTest

--- BORGCalendar/model/src/test/java/net/sf/borg/test/NoDBGetDayTests.java
+++ BORGCalendar/model/src/test/java/net/sf/borg/test/NoDBGetDayTests.java
@@ -2,6 +2,7 @@ package net.sf.borg.test;
 
 import static org.junit.Assert.*;
 
+import java.lang.ExceptionInInitializerError;
 
 import org.junit.Test;
 
@@ -9,8 +10,9 @@ import net.sf.borg.model.Day;
 
 public class NoDBGetDayTests {
 
-	@Test(expected=java.lang.ExceptionInInitializerError.class)
-	public void ClosedDatabaseConnection() throws Exception {
+	//This actually causes a null pointer exception, but something is causing JUnit to see ExceptionInInitializerError
+	@Test(expected=ExceptionInInitializerError.class)
+	public void NoDatabaseConnection() throws Exception {
 		Day d = Day.getDay(2017, 11, 9);
 		
 	}
