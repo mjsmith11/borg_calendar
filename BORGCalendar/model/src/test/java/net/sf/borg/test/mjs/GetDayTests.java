7ba3a672b11f7a7efbccb8acea31c3ff2646b6f0
Matthew Smith <mjsmith92@outlook.com>
2017-11-24 18:13:35 -0500

    Add More Holiday Tests.

--- BORGCalendar/model/src/test/java/net/sf/borg/test/mjs/GetDayTests.java
+++ BORGCalendar/model/src/test/java/net/sf/borg/test/mjs/GetDayTests.java
@@ -261,43 +261,159 @@ public class GetDayTests {
 	
 	@Test
 	public void Test20to23Halloween() throws Exception{
-		CheckDayForHoliday("true","",9,31,2017,Resource.getResourceString("Halloween")); 			//20
-		CheckDayForHoliday("false","",9,31,2017,""); 												//21
-		CheckDayForHoliday("true","",10,31,2017,""); 												//22
-		CheckDayForHoliday("true","",9,30,2017,""); 												//23
+		CheckDayForHoliday("true","",9,31,2017,Resource.getResourceString("Halloween")); 				//20
+		CheckDayForHoliday("false","",9,31,2017,""); 													//21
+		CheckDayForHoliday("true","",10,31,2017,""); 													//22
+		CheckDayForHoliday("true","",9,30,2017,""); 													//23
 	}
 	
 	@Test
 	public void Test24to27IndependenceDay() throws Exception{
-		CheckDayForHoliday("true","",6,4,2017,Resource.getResourceString("Independence_Day"));		//24
-		CheckDayForHoliday("false","",6,4,2017,"");													//25
-		CheckDayForHoliday("true","",7,4,2017,"");													//26
-		CheckDayForHoliday("true","",6,5,2017,"");													//27
+		CheckDayForHoliday("true","",6,4,2017,Resource.getResourceString("Independence_Day"));			//24
+		CheckDayForHoliday("false","",6,4,2017,"");														//25
+		CheckDayForHoliday("true","",7,4,2017,"");														//26
+		CheckDayForHoliday("true","",6,5,2017,"");														//27
 	}
 	
 	@Test
 	public void Test28to31GroundhogDay() throws Exception{
-		CheckDayForHoliday("true","",1,2,2017,Resource.getResourceString("Ground_Hog_Day"));		//28
-		CheckDayForHoliday("false","",1,2,2017,"");													//29
-		CheckDayForHoliday("true","",2,2,2017,"");													//30
-		CheckDayForHoliday("true","",1,3,2017,"");													//31
+		CheckDayForHoliday("true","",1,2,2017,Resource.getResourceString("Ground_Hog_Day"));			//28
+		CheckDayForHoliday("false","",1,2,2017,"");														//29
+		CheckDayForHoliday("true","",2,2,2017,"");														//30
+		CheckDayForHoliday("true","",1,3,2017,"");														//31
 	}
 	
 	@Test
 	public void Test32to35ValentinesDay() throws Exception{
-		CheckDayForHoliday("true","",1,14,2017,Resource.getResourceString("Valentine's_Day"));		//32
-		CheckDayForHoliday("false","",1,14,2017,"");												//33
-		CheckDayForHoliday("true","",2,14,2017,"");													//34
-		CheckDayForHoliday("true","",1,15,2017,"");													//35
+		CheckDayForHoliday("true","",1,14,2017,Resource.getResourceString("Valentine's_Day"));			//32
+		CheckDayForHoliday("false","",1,14,2017,"");													//33
+		CheckDayForHoliday("true","",2,14,2017,"");														//34
+		CheckDayForHoliday("true","",1,15,2017,"");														//35
 	}
 		
 	@Test
 	public void Test36to39StPatricksDay() throws Exception{
-		CheckDayForHoliday("true","",2,17,2017,Resource.getResourceString("St._Patrick's_Day"));	//36
-		CheckDayForHoliday("false","",2,17,2017,"");												//37
-		CheckDayForHoliday("true","",3,17,2017,"");													//38
-		CheckDayForHoliday("true","",2,18,2017,"");													//39
+		CheckDayForHoliday("true","",2,17,2017,Resource.getResourceString("St._Patrick's_Day"));		//36
+		CheckDayForHoliday("false","",2,17,2017,"");													//37
+		CheckDayForHoliday("true","",3,17,2017,"");														//38
+		CheckDayForHoliday("true","",2,18,2017,"");														//39
 	}
+	
+	@Test
+	public void Test40to43VeteransDay() throws Exception{
+		//false is needed for Canadian Holidays to exclude Remembrance Day
+		CheckDayForHoliday("true","false",10,11,2017,Resource.getResourceString("Veteran's_Day"));		//40
+		CheckDayForHoliday("false","false",10,11,2017,"");												//41
+		CheckDayForHoliday("true","",11,11,2017,"");													//42
+		CheckDayForHoliday("true","",10,12,2017,"");													//43
+	}
+	
+	@Test
+	public void Test44to47LaborDay() throws Exception{
+		//must include false for Canadian holidays to exclude Canadian Labour Day and Civic Holiday.
+		CheckDayForHoliday("true","false",8,4,2017,Resource.getResourceString("Labor_Day"));			//44
+		CheckDayForHoliday("false","false",8,4,2017,"");												//45
+		CheckDayForHoliday("true","false",7,7,2017,"");													//46
+		CheckDayForHoliday("true","",8,5,2017,"");														//47
+	}
+	
+	@Test
+	public void Test48to51MLKDay() throws Exception{
+		CheckDayForHoliday("true","",0,15,2018,Resource.getResourceString("Martin_Luther_King_Day"));	//48
+		CheckDayForHoliday("false","",0,15,2018,"");													//49
+		CheckDayForHoliday("true","",2,19,2018,"");														//50
+		CheckDayForHoliday("true","",0,16,2018,"");														//51
+	}
+	
+	@Test
+	public void Test52to55PresidentsDay() throws Exception{
+		CheckDayForHoliday("true","",1,20,2017,Resource.getResourceString("Presidents_Day"));			//52
+		CheckDayForHoliday("false","",1,20,2017,"");													//53
+		CheckDayForHoliday("true","",10,20,2017,"");													//54
+		CheckDayForHoliday("true","",1,21,2017,"");														//55
+	}
+	
+	@Test
+	public void Test56to59MemorialDay() throws Exception{
+		CheckDayForHoliday("true","",4,29,2017,Resource.getResourceString("Memorial_Day"));				//56
+		CheckDayForHoliday("false","",4,29,2017,"");													//57
+		CheckDayForHoliday("true","",5,26,2017,"");														//58
+		CheckDayForHoliday("true","",4,30,2017,"");														//59
+	}
+	
+	@Test
+	public void Test60to63ColumbusDay() throws Exception{
+		//false is required for canadian holidays because 9,9 is canadian thanksgiving.
+		CheckDayForHoliday("true","false",9,9,2017,Resource.getResourceString("Columbus_Day"));			//60
+		CheckDayForHoliday("false","false",9,9,2017,"");												//61
+		CheckDayForHoliday("true","",10,13,2017,"");													//62
+		CheckDayForHoliday("true","",9,10,2017,"");														//63
+	}
+	
+	@Test
+	public void Test64to67MothersDay() throws Exception{
+		CheckDayForHoliday("true","",4,14,2017,Resource.getResourceString("Mother's_Day"));				//64
+		CheckDayForHoliday("false","",4,14,2017,"");													//65
+		CheckDayForHoliday("true","",5,11,2017,"");														//66
+		CheckDayForHoliday("true","",4,15,2017,"");														//67
+	}
+	
+	@Test
+	public void Test68to71FathersDay() throws Exception{
+		CheckDayForHoliday("true","",5,18,2017,Resource.getResourceString("Father's_Day"));				//68
+		CheckDayForHoliday("false","",5,18,2017,"");													//69
+		CheckDayForHoliday("true","",6,16,2017,"");														//70
+		CheckDayForHoliday("true","",5,19,2017,"");														//71
+	}
+	
+	@Test
+	public void Test72to75Thanksgiving() throws Exception{
+		CheckDayForHoliday("true","",10,23,2017,Resource.getResourceString("Thanksgiving"));			//72
+		CheckDayForHoliday("false","",10,23,2017,"");													//73
+		CheckDayForHoliday("true","",11,28,2017,"");													//74
+		CheckDayForHoliday("true","",10,24,2017,"");													//75
+	}
+	
+	@Test
+	public void Test76to79CanadaDay() throws Exception{
+		CheckDayForHoliday("","true",6,1,2017,Resource.getResourceString("Canada_Day"));				//76
+		CheckDayForHoliday("","false",6,1,2017,"");														//77
+		CheckDayForHoliday("","true",7,1,2017,"");														//78
+		CheckDayForHoliday("","true",6,2,2017,"");														//79
+	}
+	
+	@Test
+	public void Test80to83BoxingDay() throws Exception{
+		CheckDayForHoliday("","true",11,26,2017,Resource.getResourceString("Boxing_Day"));				//80
+		CheckDayForHoliday("","false",11,26,2017,"");													//81
+		CheckDayForHoliday("","true",10,26,2017,"");													//82
+		CheckDayForHoliday("","true",11,27,2017,"");													//83
+	}
+	
+	@Test
+	public void Test84to87CivicHoliday() throws Exception{
+		CheckDayForHoliday("","true",7,7,2017,Resource.getResourceString("Civic_Holiday"));				//84
+		CheckDayForHoliday("","false",7,7,2017,"");														//85
+		CheckDayForHoliday("","true",10,6,2017,"");														//86
+		CheckDayForHoliday("","true",7,8,2017,"");														//87
+	}
+	
+	@Test
+	public void Test88to91RemembranceDay() throws Exception{
+		CheckDayForHoliday("false","true",10,11,2017,Resource.getResourceString("Remembrance_Day"));	//88
+		CheckDayForHoliday("false","false",10,11,2017,"");												//89
+		CheckDayForHoliday("","true",11,11,2017,"");													//90
+		CheckDayForHoliday("","true",10,12,2017,"");													//91
+	}
+	
+	@Test
+	public void Test92to95LabourDayCan() throws Exception{
+		CheckDayForHoliday("false","true",8,4,2017,Resource.getResourceString("Labour_Day_(Can)"));		//92
+		CheckDayForHoliday("false","false",8,4,2017,"");												//93
+		CheckDayForHoliday("","true",9,2,2017,"");														//94
+		CheckDayForHoliday("","true",8,5,2017,"");														//95
+	}
+	
 	
 	///Helper Method for Predicate Analysis Tests
 	///1. Set SHOWUSHOLIDAYS to showUS if it isn't ""
@@ -307,8 +423,8 @@ public class GetDayTests {
 	///5. If expectedHoliday is not "", assert that the specified holiday is returned on the day
 	///6. Restore SHOWUSHOLIDAYS and SHOWCANHOLIDAYS if they were changed
 	private void CheckDayForHoliday(String showUS, String showCAN, int month, int day, int year, String expectedHoliday) throws Exception {
-		String backupShowUS;
-		String backupShowCAN;
+		String backupShowUS="";
+		String backupShowCAN="";
 		if(!showUS.equals("")) {
 			backupShowUS = Prefs.getPref(PrefName.SHOWUSHOLIDAYS);
 			Prefs.putPref(PrefName.SHOWUSHOLIDAYS, showUS);
@@ -327,6 +443,13 @@ public class GetDayTests {
 		{
 			String observedItemLabel = ((TreeSet<CalendarEntity>) d.getItems()).first().getText();
 			assertEquals("Expected holiday not found",expectedHoliday,observedItemLabel);
+		}
+		
+		if(!showUS.equals("")) {;
+			Prefs.putPref(PrefName.SHOWUSHOLIDAYS, backupShowUS);
+		}
+		if(!showCAN.equals("")) {
+			Prefs.putPref(PrefName.SHOWCANHOLIDAYS, backupShowCAN);
 		}
 	}
 
