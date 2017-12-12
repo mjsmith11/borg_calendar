434caa947f38b94996f1c1a93595abc6f23cecdd
Matt Smith <mjsmith17@bsu.edu>
2017-12-11 22:13:16 -0500

    Add Empty parseOutDatesTestsV2

---
+++ BORGCalendar/model/src/test/java/net/sf/borg/test/mjs/parseOutDatesTestsV2.java
@@ -0,0 +1,10 @@
+package net.sf.borg.test.mjs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class parseOutDatesTestsV2 {
+
+	//No tests were needed to improve the coverage of the original test set
+}
